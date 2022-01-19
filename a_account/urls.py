b725ef446e2a177163a2f8d2be904be6ff8ad79d
Jacky Wong <jackywong.co@outlook.com>
2022-01-19 23:28:04 +0800

    url update

--- a_account/urls.py
+++ a_account/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
 
     path('register/', RegisterView.as_view(), name="register"),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('logout/', LogoutView.as_view(), name="logout"),
 
 ]
