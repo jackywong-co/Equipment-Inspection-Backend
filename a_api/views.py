2164e51f006a24701bf1a1b98f64411a9f5e6293
Jacky Wong <chikonwong@gmail.com>
2022-01-20 09:02:46 +0800

    Update views.py

--- a_api/views.py
+++ a_api/views.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 from django.http import Http404
 from rest_framework.response import Response
@@ -70,5 +70,3 @@ class EquipmentView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
->>>>>>> parent of 17992b0 (no message)
