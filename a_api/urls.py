7b8d8cc8b21c419e4b28ce1d7d753d85adb28e94
Jacky Wong <jackywong.co@outlook.com>
2022-01-21 12:26:18 +0800

    updated url

--- a_api/urls.py
+++ a_api/urls.py
@@ -1,25 +1,6 @@
-
 from django.urls import path, include
 
-from a_form.views import RoomView, RoomDetailView, EquipmentView, UserView, FormView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
 urlpatterns = [
-    path('auth/', include('a_account.urls')),
-
-    path('user/', UserView.as_view()),
-    path('room/', RoomView.as_view()),
-    path('room/<str:pk>', RoomDetailView.as_view()),
-    path('equipment/', EquipmentView.as_view()),
-
-    path('form/', FormView.as_view()),
-    # path('test/', ExampleView.as_view()),
-
-    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
+    path('', include('a_account.urls')),
+    path('', include('a_form.urls')),
 ]
--- a_form/urls.py
+++ a_form/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
+from a_form.views import RoomView, RoomDetailView, EquipmentView, UserView, FormView
 
-from a_form.views import SignInView, DashboardView
 
 urlpatterns = [
-    path('', SignInView.as_view(), name='home'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard')
+
+    path('user/', UserView.as_view()),
+    path('room/', RoomView.as_view()),
+    path('room/<str:pk>', RoomDetailView.as_view()),
+    path('equipment/', EquipmentView.as_view()),
+
+    path('form/', FormView.as_view()),
+    # path('', SignInView.as_view(), name='home'),
+    # path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
