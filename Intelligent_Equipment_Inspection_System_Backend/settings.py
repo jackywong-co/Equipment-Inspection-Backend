9f4ed44dae31a3396d4bf66c75c144ee59c34a40
Jacky Wong <chikonwong@gmail.com>
2022-01-24 09:47:07 +0800

    Update settings.py

--- Intelligent_Equipment_Inspection_System_Backend/settings.py
+++ Intelligent_Equipment_Inspection_System_Backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8ed%s-eogqg6edj1gn#(!h)gwp+k5r3&gjd4oxi@t+q^fgaa*%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
