4779efda9c9b3533820ae78e22b2bb80fa25e4e1
Jacky Wong <jackywong.co@outlook.com>
2022-02-07 23:21:56 +0800

    update get user api

--- a_account/serializers.py
+++ a_account/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'is_staff')
+        fields = ('id', 'username', 'password', 'is_staff', 'is_active')
         extra_kwargs = {
             'password': {'write_only': True}
         }
